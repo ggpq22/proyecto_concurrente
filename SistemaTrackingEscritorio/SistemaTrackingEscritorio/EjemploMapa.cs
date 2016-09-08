4fadc45309d420641f87250c1030f971e57e27cd
ggpq22 <ggpq13@hotmail.com>
2016-09-08 20:06:14 -0300

    gonzalo

---
+++ SistemaTrackingEscritorio/SistemaTrackingEscritorio/EjemploMapa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.MapProviders;
+using GMap.NET;
+
+namespace Mapa
+{
+    public partial class EjemploMapa : Form
+    {
+        public EjemploMapa()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(37.583, -1.133);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 9;
+            gMapControl1.AutoScroll = true;
+        }
+    }
+}
