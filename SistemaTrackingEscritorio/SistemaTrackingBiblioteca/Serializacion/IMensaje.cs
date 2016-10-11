20eececb293d24e0bd4a809b3e2fa91ac7113aaf
Arriola Mario <arriola.mario.90@gmail.com>
2016-10-11 18:45:49 -0300

    Interface IMensaje

---
+++ SistemaTrackingEscritorio/SistemaTrackingBiblioteca/Serializacion/IMensaje.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTrackingBiblioteca.Serializacion
+{
+    public interface IMensaje
+    {
+        string From { get; set; }
+        string To { get; set; }
+
+        string Serializar();
+
+        void Deserealizar(Object o);
+    }
+}
---
+++ SistemaTrackingEscritorio/SistemaTrackingBiblioteca/Serializacion/MensajeConexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaTrackingBiblioteca.Serializacion
+{
+    public class MensajeConexion :IMensaje
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Mensaje { get; set; }
+
+        public string Serializar()
+        {
+
+            return "";
+        }
+
+
+        public void Deserealizar(Object o)
+        {
+
+        }
+    }
+}
