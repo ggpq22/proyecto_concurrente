420c2792c22703eca5ddbab1a24d2a0ffc2585c8
PabloWasHere <pablobarco@outlook.com.ar>
2016-10-11 19:02:57 -0300

    Servidor casi completo

---
+++ ServidorTracking/ServidorTracking/ServerClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace ServidorTracking
+{
+    class ServerClient
+    {
+        string name;
+        TcpClient client;
+        MessageDelivery delivery;
+        //ConcurrentQueue<>
+
+        public ServerClient(TcpClient client)
+        {
+            try 
+	        {
+                this.client = client;
+                delivery = new MessageDelivery(this.client.GetStream());
+	        }
+	        catch (Exception)
+	        {
+		        throw;
+	        }
+        }
+
+        public string RecieveFromClient()
+        {
+            try
+            {
+                return delivery.RecieveMessage();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public void SendToClient(string message)
+        {
+            try
+            {
+                delivery.SendMessage(message);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        public void CloseClient()
+        {
+            client.Close();
+        }
+    }
+}
