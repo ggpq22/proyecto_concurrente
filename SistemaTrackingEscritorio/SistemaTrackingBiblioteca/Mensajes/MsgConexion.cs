4a02aae753c97d046f5785f15600235e86a12735
Arriola Mario <arriola.mario.90@gmail.com>
2016-10-13 09:19:29 -0300

    Msg

---
+++ SistemaTrackingEscritorio/SistemaTrackingBiblioteca/Mensajes/MsgConexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SistemaTrackingBiblioteca.Mensajes
+{
+    public class MsgConexion : IMensaje
+    {
+        [JsonConstructor]
+        public MsgConexion() { }
+
+        [JsonProperty]
+        public string From { get; set; }
+        [JsonProperty]
+        public string To { get; set; }
+        [JsonProperty]
+        public DateTime Fecha { get; set; }
+        [JsonProperty]
+        public string Mensaje { get; set; }
+
+    }
+}
---
+++ SistemaTrackingEscritorio/SistemaTrackingBiblioteca/Mensajes/MsgLocalizacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SistemaTrackingBiblioteca.Mensajes
+{
+    [JsonObject]
+    class MsgLocalizacion : IMensaje
+    {
+        [JsonConstructor]
+        public MsgLocalizacion() { }
+
+        [JsonProperty]
+        public string From { get; set; }
+        
+        [JsonProperty]
+        public string To { get; set; }
+
+        [JsonProperty]
+        public DateTime Fecha { get; set; }
+    }
+}
