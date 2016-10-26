23226f2ceba5d005982f2f8b3a2323bbb67aee40
PabloWasHere <pablobarco@outlook.com.ar>
2016-10-26 15:55:06 -0300

    Base del cliente de escritorio

---
+++ SistemaTrackingEscritorio/SistemaTrackingEscritorio/MessageDelivery.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServidorTracking
+{
+    class MessageDelivery
+    {
+        StreamWriter writer;
+        StreamReader reader;
+        NetworkStream stream;
+        TcpClient client;
+
+        public TcpClient Client
+        {
+            get { return client; }
+            set 
+            {
+                client = value;
+                this.stream = client.GetStream();
+            }
+        }
+
+        public MessageDelivery(TcpClient client)
+        {
+            this.client = client;
+            this.stream = this.client.GetStream();
+        }
+
+        public void OpenDelivery()
+        {
+            try
+            {
+                writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+                reader = new StreamReader(stream, Encoding.ASCII);
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void CloseDelivery()
+        {
+            try
+            {
+                writer.Close();
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public string RecieveMessage()
+        {
+            try
+            {
+                string line = reader.ReadLine();
+                return line;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            try
+            {
+                writer.WriteLine(message);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ SistemaTrackingEscritorio/SistemaTrackingEscritorio/ServerClient.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Concurrent;
+using SistemaTrackingBiblioteca.Serializacion;
+using SistemaTrackingBiblioteca.Mensajes;
+
+namespace ServidorTracking
+{
+    class ServerClient
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        TcpClient client;
+        CommunicationService service;
+        ConcurrentQueue<Mensaje> mensajesSalida = new ConcurrentQueue<Mensaje>();
+
+        Thread sending;
+
+        // Events: Connect
+        public delegate void CommunicationEventHandler(object sender, Mensaje mensaje);
+        public event CommunicationEventHandler Connect;
+        protected virtual void OnConnect(Mensaje e)
+        {
+            if (Connect != null)
+                Connect(this, e);
+        }
+        // Events: Disconnect
+        public event CommunicationEventHandler Disconnect;
+        protected virtual void OnDisconnect(Mensaje e)
+        {
+            if (Disconnect != null)
+                Disconnect(this, e);
+        }
+
+        // Events: LocationChanged
+        public event CommunicationEventHandler LocationChanged;
+        protected virtual void OnLocationChanged(Mensaje e)
+        {
+            if (LocationChanged != null)
+                LocationChanged(this, e);
+        }
+
+        // Event Methods
+        void service_Connect(object sender, Mensaje message)
+        {
+            MsgConexion msn = message as MsgConexion;
+            this.name = msn.From;
+            OnConnect(msn);
+        }
+        void service_Disconnect(object sender, Mensaje message)
+        {
+            MsgConexion msn = message as MsgConexion;
+            OnDisconnect(msn);
+            CloseClient();
+        }
+        void service_LocationChanged(object sender, Mensaje message)
+        {
+            MsgLocalizacion msn = message as MsgLocalizacion;
+            OnLocationChanged(msn);
+        }
+
+        public ServerClient(TcpClient client)
+        {
+            this.client = client;
+            service = new CommunicationService(this.client.GetStream(), this.client);
+	        
+            // Subscribe Events
+            service.Connect += service_Connect;
+            service.Disconnect += service_Disconnect;
+            service.LocationChanged += service_LocationChanged;
+
+            sending = new Thread(sendMessages);
+            sending.Start();
+        }
+
+        public void SendToServer(Mensaje message)
+        {
+            mensajesSalida.Enqueue(message);
+        }
+
+        public void CloseClient()
+        {
+            service.CloseCommunications();
+            client.Close();
+        }
+
+        private void sendMessages()
+        {
+            while (true)
+            {
+                if (mensajesSalida.Count > 0)
+                {
+                    Mensaje m;
+
+                    while (mensajesSalida.TryDequeue(out m))
+                    {
+                        service.SendToServer(m);
+                    }
+                }
+            }
+        }
+    }
+}
